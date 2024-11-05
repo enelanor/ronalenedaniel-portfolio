b94c1defd3092c70e7b9066c6146bf75a97588de
enelanor <ronalenedaniel@yahoo.com>
2024-11-04 16:16:12 -0800

    2024/11/04 changes

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,42 @@
 // scroll effect
 AOS.init();
+
+// glider
+document.addEventListener('DOMContentLoaded', function () {
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '#dots',
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [
+            {
+                // Screens greater than >= 775px
+                breakpoint: 775,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                // Screens greater than >= 576px
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                // Screens less than < 576px
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+});
